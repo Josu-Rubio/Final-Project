33ec57ff0059463fd1aadf2daa6c9baec100b4ba
Josué Rubio <josu_mr@hotmail.com>
2020-07-14 19:48:40 +0200

    Creating user and login on Redux

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+import { setCurrentUser, logoutUser } from '../actions/authActions';
 
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -7,8 +10,29 @@ import store from '../store';
 import { NavBar, Landing } from '../components/layout';
 import { Register, Login } from '../components/auth';
 import { ProductsList, CreateAd, UpdateAd } from '../pages';
+import PrivateRoute from '../components/private-route/PrivateRoute';
+import Dashboard from '../components/dashboard/Dashboard';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = './login';
+  }
+}
 
 function App() {
   return (
@@ -19,9 +43,10 @@ function App() {
           <Route exact path='/' component={Landing} />
           <Route exact path='/register' component={Register} />
           <Route exact path='/login' component={Login} />
-          <Route path='/products/list' exact component={ProductsList} />
-          <Route path='/products/create' exact component={CreateAd} />
-          <Route path='/products/update/:id' exact component={UpdateAd} />
+          <PrivateRoute exact path='/products/list' component={ProductsList} />
+          <PrivateRoute exact path='/products/create' component={CreateAd} />
+          <Route exact path='/products/update/:id' component={UpdateAd} />
+          <PrivateRoute exact path='/dashboard' component={Dashboard} />
         </Switch>
       </Router>
     </Provider>
